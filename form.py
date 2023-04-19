bd9b8b0747041282f4d7783d910538fc0fadb684
YUMOTO Michitaka <michitaka@quality-start.in>
2023-04-19 20:59:34 +0900

    ライブラリの最新化

--- form.py
+++ form.py
@@ -1,8 +1,9 @@
-from flask_wtf import Form
-from wtforms import StringField,PasswordField
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, Length
 
-class SignupForm(Form):
+
+class SignupForm(FlaskForm):
     username = StringField('ユーザー名', validators=[
         DataRequired(message='ユーザー名が未登録です'),
         Length(max=10,message='ユーザー名は10文字までにして下さい')
@@ -16,7 +17,7 @@ class SignupForm(Form):
     ])
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Eメールアドレス', validators=[
         Email(message='メールアドレスが未入力か、正しいアドレスではありません'),
     ])
