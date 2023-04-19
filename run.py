ee152cdf6e53317390dd5bc0ac75b5417ff84d6d
YUMOTO Michitaka <michitaka@quality-start.in>
2023-04-19 20:40:52 +0900

    update: ライブラリ最新化、ポート番号の変更

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xNVg}f_m:UmiOB{9bC`SvB9j5N<-3I./'
 
@@ -13,4 +14,4 @@ def schoo():
     return render_template('schoo.html',message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=5200)
